48947c052680441638bbb99e62bdc949aa5f0d05
Nicoguzok <near.ng7@gmail.com>
2025-02-27 18:49:04 +0100

    Casi terminada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,20 @@
 const navToggle = document.getElementById('bar-toggle');
 const navBar = document.getElementById('nav');
+const navLinks = navBar.querySelectorAll('.nav-bar a'); // Selecciona todos los enlaces dentro de nav-bar
 
 navToggle.addEventListener('click', () => {
     navBar.classList.toggle('active');
+    updateNavToggleIcon();
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navBar.classList.remove('active'); // Cierra el menú
+        updateNavToggleIcon(); // Actualiza el icono
+    });
+});
+
+function updateNavToggleIcon() {
     if (navBar.classList.contains('active')) {
         navToggle.classList.remove('fa-bars');
         navToggle.classList.add('fa-xmark');
@@ -10,4 +22,4 @@ navToggle.addEventListener('click', () => {
         navToggle.classList.remove('fa-xmark');
         navToggle.classList.add('fa-bars');
     }
-});
+}
